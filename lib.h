1aa64ee954c993a8d1fa2b15e3e085be5dd30aaa
diegonuunez <diegonunco12@gmail.com>
2025-11-18 15:01:16 +0100

    Add headers functions

--- lib.h
+++ lib.h
@@ -3,6 +3,8 @@
 #ifndef LIB_H
 #define LIB_H
 
- 
+char* capturaCommand(char* command);
+FILE* cargaArchivo(FILE* file);
+int gestionCommand(char* command);
 
 #endif /* LIB_H */
--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include "lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-    return 0;
+
+    char* command;
+    while(1)
+    {  
+        scanf("%99s",command);
+        if(strcmp(command,"exit") == 0)
+        {
+            exit(-1);
+        }
+    }
 }
